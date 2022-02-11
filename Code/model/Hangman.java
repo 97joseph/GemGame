4471613f8e0c96e62dab7e002d88209d1bba1fb0
josephkibira <33089347+97joseph@users.noreply.github.com>
2022-02-11 21:24:32 +0300

    R

---
+++ Code/model/Hangman.java
@@ -0,0 +1,24 @@
+package model;
+import utility.WordList;
+
+public class Hangman {
+    private String key;
+    private int health = 5;
+    
+    public Hangman() {
+        WordList k = new WordList();
+        key = k.getWord();
+    }
+
+    public int getHealth() {
+        return health; 
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+
+}
+//Code Output Model
+
